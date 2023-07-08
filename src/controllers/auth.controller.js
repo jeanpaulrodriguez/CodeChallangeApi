85965b2f47ef0d15747600aab65bd1b7029fe7da
Jean Paul Rodriguez <jeanpaulrodriguezb@gmail.com>
2023-07-08 03:42:58 -0400

    added endpoint login and logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,10 +22,36 @@ export const register = async (req, res) => {
       username: userSaved.username,
       email: userSaved.email,
     });
-    res.send("registrando");
   } catch (error) {
     console.log(error);
   }
 };
 
-export const login = (req, res) => res.send("logueado");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "user not found" });
+
+    const matching = bcrypt.compare(password, userFound.password);
+    if (!matching) return res.status(400).json({ message: "invalid password" });
+
+    const token = await createToken({ id: userFound._id });
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
