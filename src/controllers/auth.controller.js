19c7c380eaacdf3b2c9542edc375b74684e25431
Jean Paul Rodriguez <jeanpaulrodriguezb@gmail.com>
2023-07-08 02:51:50 -0400

    added webtoken to users

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,27 @@
+import { createToken } from "../libs/jwt.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
+    const hash = await bcrypt.hash(password, 10);
+
     const createUser = new User({
       username,
       email,
-      password,
+      password: hash,
     });
     const userSaved = await createUser.save();
-    res.json(userSaved);
+
+    const token = await createToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
     res.send("registrando");
   } catch (error) {
     console.log(error);
